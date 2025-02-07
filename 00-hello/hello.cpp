41bbee0c350bb3cd4a325099ff97e077092df811
wlk <wlk@github.user.com>
2025-02-07 17:24:29 +0800

    vscode WebAssembly intellisense

--- 00-hello/hello.cpp
+++ 00-hello/hello.cpp
@@ -1,8 +1,11 @@
 
 #include <iostream>
+#include <emscripten.h>
 
 int main() 
 {
+
+    emscripten_run_script("console.log('Hello emscripten')");
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
