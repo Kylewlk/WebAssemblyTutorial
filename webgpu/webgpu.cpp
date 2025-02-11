d9f9caa57b83caed5682d6d08bdb7592829e0730
wlk1229 <wlk1229@sina.com>
2025-02-11 23:32:32 +0800

    webgpu draw texture

--- webgpu/webgpu.cpp
+++ webgpu/webgpu.cpp
@@ -6,11 +6,23 @@
 #include <thread>
 #include <string>
 
-#define LOGI(...) do{ std::cout << std::format(__VA_ARGS__) << std::endl; }while (false)
+#define LOGI(...)                                           \
+    do                                                      \
+    {                                                       \
+        std::cout << std::format(__VA_ARGS__) << std::endl; \
+    } while (false)
 // error
-#define LOGE(...) do{ std::cerr << std::format(__VA_ARGS__) << std::endl; }while (false)
+#define LOGE(...)                                           \
+    do                                                      \
+    {                                                       \
+        std::cerr << std::format(__VA_ARGS__) << std::endl; \
+    } while (false)
 // warning
-#define LOGW(...) do{ std::cerr << std::format(__VA_ARGS__) << std::endl; }while (false)
+#define LOGW(...)                                           \
+    do                                                      \
+    {                                                       \
+        std::cerr << std::format(__VA_ARGS__) << std::endl; \
+    } while (false)
 
 const std::string canvasID = "canvas"; // <canvas> id
 WGPUInstance instance{};
@@ -22,8 +34,11 @@ WGPUTextureFormat preferredFormat = WGPUTextureFormat_RGBA8Unorm;
 
 WGPURenderPipeline pipeline{};
 WGPUBindGroupLayout bindGroupLayout{};
+WGPUBindGroup bindGroup{};
 WGPUSampler sampler{};
 WGPUBuffer vertexBuffer{};
+WGPUTexture texture{};
+WGPUTextureView textureView{};
 
 void initWebGpu()
 {
@@ -56,13 +71,13 @@ void initWebGpu()
             }
             adapter = ad;
             done = true;
-        }, nullptr
-    );
+        },
+        nullptr);
     while (!done)
     {
         emscripten_sleep(10);
     }
-    
+
     WGPUAdapterProperties adapterProperties = {};
     wgpuAdapterGetProperties(adapter, &adapterProperties);
 
@@ -84,8 +99,8 @@ void initWebGpu()
             }
             device = de;
             done = true;
-        }, nullptr
-    );
+        },
+        nullptr);
     while (!done)
     {
         emscripten_sleep(10);
@@ -121,9 +136,7 @@ void initWebGpu()
             default:
                 break;
             }
-            std::cerr << "Device Uncaptured Error: " << errorType << ", " << message << std::endl;
-        },nullptr
-    );
+            std::cerr << "Device Uncaptured Error: " << errorType << ", " << message << std::endl; }, nullptr);
 
     // config surface
     {
@@ -141,54 +154,28 @@ void initWebGpu()
     }
 }
 
-bool render(double time, void *userData)
-{
-    WGPUSurfaceTexture surfaceTexture{};
-    wgpuSurfaceGetCurrentTexture(surface, &surfaceTexture);
-    if (surfaceTexture.status != WGPUSurfaceGetCurrentTextureStatus_Success)
-    {
-        return true;
-    }
-    WGPUTextureView renderView = wgpuTextureCreateView(surfaceTexture.texture, nullptr);
-    wgpuTextureRelease(surfaceTexture.texture);
-
-    auto commandEncoder = wgpuDeviceCreateCommandEncoder(device, nullptr);
-    wgpuCommandEncoderSetLabel(commandEncoder, "RenderCommand");
-
-    WGPURenderPassDescriptor descriptor = {};
-
-    WGPURenderPassColorAttachment colorAttachment{};
-    colorAttachment.view = renderView;
-    colorAttachment.loadOp = WGPULoadOp_Clear;
-    colorAttachment.storeOp = WGPUStoreOp_Store;
-    colorAttachment.clearValue = {0.0f, 0.6f, 0.8f, 1.0f};
-#ifndef WEBGPU_BACKEND_WGPU
-    colorAttachment.depthSlice = WGPU_DEPTH_SLICE_UNDEFINED;
-#endif
-    descriptor.colorAttachmentCount = 1;
-    descriptor.colorAttachments = &colorAttachment;
-    auto renderPass = wgpuCommandEncoderBeginRenderPass(commandEncoder, &descriptor);
-
-    wgpuRenderPassEncoderEnd(renderPass);
-
-    auto command = wgpuCommandEncoderFinish(commandEncoder, nullptr);
-    wgpuQueueSubmit(queue, 1, &command);
-    wgpuCommandBufferRelease(command);
-    wgpuCommandEncoderRelease(commandEncoder);
-
-    wgpuTextureViewRelease(renderView);
-
-    // wgpuSurfacePresent(surface); // 浏览器内部会调用present，无需再调用
-    return true; // 返回true继续回调
-}
-
 constexpr const char *shaderSource = R"(
-@vertex fn vs(@location(0) pos : vec4f, @location(1) uv : vec2f) -> @builtin(position) vec4f {
-    return pos;
+
+struct VertexOutput
+{
+	@builtin(position) position: vec4f,
+	@location(0) uv: vec2f,
 }
 
-@fragment fn fs(@location(0) uv: vec2f) -> @location(0) vec4f {
-    return vec4f(uv, 0, 1);
+@group(0) @binding(0) var mySampler: sampler;
+@group(0) @binding(1) var myTexture: texture_2d<f32>;
+
+@vertex fn vs(@location(0) pos : vec4f,
+              @location(1) uv : vec2f
+            ) -> VertexOutput {
+    var output : VertexOutput;
+    output.position = pos;
+    output.uv = uv;
+    return output;
+}
+
+@fragment fn fs(@location(0) uv: vec2f,) -> @location(0) vec4f {
+    return textureSample(myTexture, mySampler, uv);
 }
 )";
 
@@ -205,9 +192,99 @@ WGPUShaderModule createShaderModule(const char *source)
     return wgpuDeviceCreateShaderModule(device, &shaderDescriptor);
 }
 
-
 void initDraw()
 {
+   
+    {
+        constexpr float vertexData[] = {
+            -0.5f, 0.5f, 0.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f, 1.0f,
+            0.5f, -0.5f, 1.0f, 1.0f,
+
+            0.5f, -0.5f, 1.0f, 1.0f,
+            0.5f, 0.5f, 1.0f, 0.0f,
+            -0.5f, 0.5f, 0.0f, 0.0f,
+        };
+        WGPUBufferDescriptor descriptor{};
+        descriptor.usage = WGPUBufferUsage_Vertex | WGPUBufferUsage_CopyDst;
+        descriptor.size = sizeof(vertexData);
+        vertexBuffer = wgpuDeviceCreateBuffer(device, &descriptor);
+        wgpuQueueWriteBuffer(queue, vertexBuffer, 0, vertexData, sizeof(vertexData));
+    }
+
+    {
+        WGPUTextureDescriptor textureDesc{};
+        textureDesc.dimension = WGPUTextureDimension_2D;
+        textureDesc.size = {4, 4, 1};
+        textureDesc.mipLevelCount = 1;
+        textureDesc.sampleCount = 1;
+        textureDesc.format = WGPUTextureFormat_RGBA8Unorm;
+        textureDesc.usage = WGPUTextureUsage_CopyDst | WGPUTextureUsage_TextureBinding;
+        textureDesc.viewFormatCount = 0;
+        textureDesc.viewFormats = nullptr;
+        texture = wgpuDeviceCreateTexture(device, &textureDesc);
+
+        WGPUImageCopyTexture destination{};
+        destination.texture = texture;
+        destination.mipLevel = 0;
+        destination.origin = {0, 0, 0};
+        destination.aspect = WGPUTextureAspect_All;
+
+        WGPUTextureDataLayout source;
+        source.offset = 0;
+        source.bytesPerRow = 4 * 4;
+        source.rowsPerImage = 4;
+
+        std::array<uint8_t, 4 * 4 * 4> imageData = {};
+        imageData.fill(255);
+        wgpuQueueWriteTexture( queue, &destination, imageData.data(), imageData.size(), &source, &textureDesc.size);
+        textureView = wgpuTextureCreateView(texture, nullptr);
+
+        WGPUSamplerDescriptor samplerDescriptor{};
+        samplerDescriptor.addressModeU = WGPUAddressMode_Repeat; 
+        samplerDescriptor.addressModeV = WGPUAddressMode_Repeat;
+        samplerDescriptor.addressModeW = WGPUAddressMode_Repeat;
+        samplerDescriptor.magFilter = WGPUFilterMode_Nearest;
+        samplerDescriptor.minFilter = WGPUFilterMode_Nearest;
+        samplerDescriptor.mipmapFilter = WGPUMipmapFilterMode_Nearest;
+        samplerDescriptor.lodMinClamp = 0.0f;
+        samplerDescriptor.lodMaxClamp = 1.0f;
+        samplerDescriptor.compare = WGPUCompareFunction_Undefined;
+        samplerDescriptor.maxAnisotropy = 1;
+        sampler = wgpuDeviceCreateSampler(device, &samplerDescriptor);
+
+        // group layout
+        std::array<WGPUBindGroupLayoutEntry, 2> bindLayoutEntries{};
+        auto &samplerLayoutEntry = bindLayoutEntries[0];
+        samplerLayoutEntry.binding = 0;
+        samplerLayoutEntry.visibility = WGPUShaderStage_Fragment;
+        samplerLayoutEntry.sampler.type = WGPUSamplerBindingType_Filtering;
+        auto &imageLayoutEntry = bindLayoutEntries[1];
+        imageLayoutEntry.binding = 1;
+        imageLayoutEntry.visibility = WGPUShaderStage_Fragment;
+        imageLayoutEntry.texture.sampleType = WGPUTextureSampleType_Float;
+        imageLayoutEntry.texture.viewDimension = WGPUTextureViewDimension_2D;
+        WGPUBindGroupLayoutDescriptor bgLayoutDescriptor{};
+        bgLayoutDescriptor.entryCount = bindLayoutEntries.size();
+        bgLayoutDescriptor.entries = bindLayoutEntries.data();
+        bindGroupLayout = wgpuDeviceCreateBindGroupLayout(device, &bgLayoutDescriptor);
+
+        // Binding Group
+        std::array<WGPUBindGroupEntry, 2> bindGroupEntries{};
+        auto &samplerEntry = bindGroupEntries[0];
+        samplerEntry.binding = 0;
+        samplerEntry.sampler = sampler;
+        auto &imageEntry = bindGroupEntries[1];
+        imageEntry.binding = 1;
+        imageEntry.textureView = textureView;
+
+        WGPUBindGroupDescriptor bgDescriptor{};
+        bgDescriptor.entryCount = bindGroupEntries.size();
+        bgDescriptor.entries = bindGroupEntries.data();
+        bgDescriptor.layout = bindGroupLayout;
+        bindGroup = wgpuDeviceCreateBindGroup(device, &bgDescriptor);
+    }
+
     {
         auto shaderModule = createShaderModule(shaderSource);
 
@@ -243,14 +320,14 @@ void initDraw()
 
         WGPUFragmentState fragmentState{};
         fragmentState.module = shaderModule;
-        fragmentState.entryPoint = "vs";
+        fragmentState.entryPoint = "fs";
         fragmentState.targetCount = 1;
         fragmentState.targets = &colorTargetState;
 
         WGPURenderPipelineDescriptor descriptor = {};
         descriptor.label = "DrawPicture";
         descriptor.vertex.module = shaderModule;
-        descriptor.vertex.entryPoint = "fs";
+        descriptor.vertex.entryPoint = "vs";
         descriptor.vertex.bufferCount = 1;
         descriptor.vertex.buffers = &vertexBuffer;
         descriptor.primitive.topology = WGPUPrimitiveTopology_TriangleList;
@@ -263,41 +340,71 @@ void initDraw()
         descriptor.multisample.alphaToCoverageEnabled = false;
         descriptor.fragment = &fragmentState;
 
+        WGPUPipelineLayoutDescriptor layoutDesc{};
+        layoutDesc.bindGroupLayoutCount = 1;
+        layoutDesc.bindGroupLayouts = &bindGroupLayout;
+        descriptor.layout = wgpuDeviceCreatePipelineLayout(device, &layoutDesc);
+
         pipeline = wgpuDeviceCreateRenderPipeline(device, &descriptor);
-    }
 
+        wgpuShaderModuleRelease(shaderModule);
+        wgpuPipelineLayoutRelease(descriptor.layout);
+    }
+}
+
+
+bool render(double time, void *userData)
+{
+    WGPUSurfaceTexture surfaceTexture{};
+    wgpuSurfaceGetCurrentTexture(surface, &surfaceTexture);
+    if (surfaceTexture.status != WGPUSurfaceGetCurrentTextureStatus_Success)
     {
-        constexpr float vertexData[] = {
-            -0.5f, 0.5f, 0.0f, 0.0f,
-            -0.5f, -0.5f, 0.0f, 1.0f,
-            0.5f, -0.5f, 1.0f, 1.0f,
-
-            0.5f, -0.5f, 1.0f, 1.0f,
-            0.5f, 0.5f, 1.0f, 0.0f,
-            -0.5f, 0.5f, 0.0f, 0.0f,
-        };
-        WGPUBufferDescriptor descriptor{};
-        descriptor.usage = WGPUBufferUsage_Vertex | WGPUBufferUsage_CopyDst;
-        descriptor.size = sizeof(vertexData);
-        vertexBuffer = wgpuDeviceCreateBuffer(device, &descriptor);
-        wgpuQueueWriteBuffer(queue, vertexBuffer, 0, vertexData, sizeof(vertexData));
+        return true;
     }
+    WGPUTextureView renderView = wgpuTextureCreateView(surfaceTexture.texture, nullptr);
+    wgpuTextureRelease(surfaceTexture.texture);
 
-    {
+    auto commandEncoder = wgpuDeviceCreateCommandEncoder(device, nullptr);
+    wgpuCommandEncoderSetLabel(commandEncoder, "RenderCommand");
 
-    }
+    WGPURenderPassDescriptor descriptor = {};
 
+    WGPURenderPassColorAttachment colorAttachment{};
+    colorAttachment.view = renderView;
+    colorAttachment.loadOp = WGPULoadOp_Clear;
+    colorAttachment.storeOp = WGPUStoreOp_Store;
+    colorAttachment.clearValue = {0.0f, 0.6f, 0.8f, 1.0f};
+#ifndef WEBGPU_BACKEND_WGPU
+    colorAttachment.depthSlice = WGPU_DEPTH_SLICE_UNDEFINED;
+#endif
+    descriptor.colorAttachmentCount = 1;
+    descriptor.colorAttachments = &colorAttachment;
+    auto renderPass = wgpuCommandEncoderBeginRenderPass(commandEncoder, &descriptor);
+
+    wgpuRenderPassEncoderSetPipeline(renderPass, pipeline);
+    wgpuRenderPassEncoderSetBindGroup(renderPass, 0, bindGroup, 0, nullptr);
+    wgpuRenderPassEncoderSetVertexBuffer(renderPass, 0, vertexBuffer, 0, wgpuBufferGetSize(vertexBuffer));
+    wgpuRenderPassEncoderDraw(renderPass, 6, 1, 0, 0);
+
+    wgpuRenderPassEncoderEnd(renderPass);
+
+    auto command = wgpuCommandEncoderFinish(commandEncoder, nullptr);
+    wgpuQueueSubmit(queue, 1, &command);
+    wgpuCommandBufferRelease(command);
+    wgpuCommandEncoderRelease(commandEncoder);
+
+    wgpuTextureViewRelease(renderView);
+
+    // wgpuSurfacePresent(surface); // 浏览器内部会调用present，无需再调用
+    return true; // 返回true继续回调
 }
 
 int main()
 {
     initWebGpu();
+    emscripten_sleep(10);
+    initDraw();
 
     emscripten_request_animation_frame_loop(render, nullptr);
     return 0;
 }
-
-
-
-
-
