6814d5ceafed39437a95aa1edd237e17efbb7859
wlk <wlk@github.user.com>
2025-10-16 15:28:04 +0800

    update hello

--- 00-hello/hello.cpp
+++ 00-hello/hello.cpp
@@ -1,19 +1,24 @@
 
 #include <iostream>
 #include <emscripten.h>
+#include <thread>
+std::thread t ;
 
-void fun( int n)
+void fun(int n)
 {
     std::cout << "Call fun: " << n << std::endl;
 }
 
 
 
-int main() 
+int main()
 {
     emscripten_run_script("console.log('Hello emscripten')");
     std::cout << "Hello, World!" << std::endl;
- 
+    t = std::thread([]()
+                    { std::cout << "Hello from a thread!" << std::endl; });
+    t.join();
+
     std::cout << "sizeof(int*): " << sizeof(int*) << std::endl;
 
     fun(5);
